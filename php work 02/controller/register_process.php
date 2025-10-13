116a073ff39c121f959b4adeca3270df5ddbcd5a
Moeen Uddin <m.moeen.u9@gmail.com>
2025-10-13 22:01:44 +0600

    Update: errors validating

--- php work 02/controller/register_process.php
+++ php work 02/controller/register_process.php
@@ -3,33 +3,49 @@
 session_start();
 include_once 'db.php';
 
-if(isset($_POST['register'])) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = md5($_POST['passworrd']);
-    $confirm_password = md5($_POST['confirm_password']);
-    $phone = $_POST['phone'];
+if (isset($_POST['register'])) {
+    $first_name = trim($_POST['first_name']);
+    $last_name = trim($_POST['last_name']);
+    $email = trim($_POST['email']);
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+    $phone = trim($_POST['phone']);
+    $gender = $_POST['gender'] ?? '';
+    $image = $_FILES['user_image'] ?? null;
 
-    $error = array();
+    $errors = [];
 
-    if(empty($name)){
-        $error['name_error'] = "Please Insert Name!";
+    // first name error
+    if (empty($first_name) || !preg_match("/^[a-zA-Z]+$/", $first_name)) {
+        $errors[] = "First Name is required and must be contain only letters!";
     }
 
-    if(empty($email)) {
-        $error['email_error'] = "Please Insert Email!";
+    // last name error
+    if (empty($last_name) || !preg_match("/^[a-zA-Z]+$/", $last_name)) {
+        $errors[] = "First Name is required and must be contain only letters!";
     }
 
-    if(empty($password)) {
-        $error['password_error'] = "Please Insert Password!";
+    // for valid email
+    if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Valid email is required!";
     }
 
-    if($password !== $confirm_password) {
-        $error['confirm_password_error'] = "Do not match password!";
+    // for password error
+    if (strlen($password) < 8) {
+        $errors[] = "Password must be at least 8 characters!";
     }
 
-    if(empty($phone)) {
-        $errorr['phone_error'] = "Please Inter Phone Number!";
+    if ($password !== $confirm_password) {
+        $error[] = "Password do not mathched!";
+    }
+
+    // for phone error
+    if (empty($phone) || !preg_match("/^[0-9]{10,15}$/", $phone)) {
+        $errorr[] = "Please Inter valid phone number!";
+    }
+
+    if (!in_array($gender, ['Male', 'Female', 'Other'])) {
+        $errors[] = "Gender is required";
     }
 
     if (empty($error)) {
@@ -46,9 +62,21 @@ if(isset($_POST['register'])) {
 
         // crerate Table if not exists
         $createTable = "
-        
+            CREATE TABLE IF NOT EXISTS `admins` (
+            `id` INT(11) AUTO_INCREMENT PRIMARY KEY,
+            `first_name` VARCHAR(50) NOT NULL,
+            `last_name` VARCHAR(50) NOT NULL,
+            `email` VARCHAR(100) NOT NULL UNIQUE,
+            `password` VARCHAR(255) NOT NULL,
+            `phone` VARCHAR(20) NOT NULL,
+            `gender` ENUM('Male', 'Female', 'Other') NOT NULL,
+            `image` VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
         ";
+
+        if ($conn->query($createTable) !== TRUE) {
+            die ("Table Creation Failed:" . $conn->error);
+        }
     }
 }
-
-?>
