96765517cefa548f1b2004590e7c7032264e4ebe
Moeen Uddin <m.moeen.u9@gmail.com>
2025-10-15 17:36:04 +0600

    Update: php-crud table design

---
+++ php-crud/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Crud</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <table class="table table-success table-striped">
+                    <thead>
+                        <tr class="">
+                            <th scope="col">#</th>
+                            <th scope="col">First Name</th>
+                            <th scope="col">Last Name</th>
+                            <th scope="col">User Email</th>
+                            <th scope="col">User Phone</th>
+                            <th scope="col">User Address</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td scope="row">#</td>
+                            <td>Moeen</td>
+                            <td>Uddin</td>
+                            <td>moeen@gmail.com</td>
+                            <td>+8801756376816</td>
+                            <td>Dhaka, Bangladesh</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
